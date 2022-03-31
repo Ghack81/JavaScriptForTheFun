d69599f259ec1941517e68957eb97485531df6f7
Ghack81 <herve-mention@hotmail.com>
2022-03-31 16:18:58 +0200

    Folder JS Projets

---
+++ Projet-JavaScript/Let.js
@@ -0,0 +1,14 @@
+//Déclaration de variables
+// Let a une portée, on l'utilise dans des block IF...
+let ab = 5;
+
+if(9==9){
+    let ab = "Maman";
+    console.log(ab);
+}
+console.log(ad);
+
+//-------------------------------------------------------------//
+// PROMPT
+var firstName = prompt("Quelle est votre nom ?");
+console.log(firstName);
