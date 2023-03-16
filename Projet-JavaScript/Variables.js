e5bb67b434ee5c16cb4757210cb20ec02c269986
Ghack81 <herve-mention@hotmail.com>
2023-03-16 17:12:55 +0100

    Fichier Type de variable JS

---
+++ Projet-JavaScript/Variables.js
@@ -0,0 +1,15 @@
+// Résumer des différents type de variable en JS
+/*
+Il existe 8 types de données de base en JavaScript.
+
+Sept types de données primitifs :
+number pour les nombres de toute nature : entier ou virgule flottante, les nombres entiers sont limités à ±(253-1).
+bigint pour des nombres entiers de longueur arbitraire.
+string pour les chaînes de caractères. Une chaîne de caractères peut avoir zéro ou plusieurs caractères, il n’y a pas de type à caractère unique distinct.
+boolean pour true/false (vrai/faux).
+null pour les valeurs inconnues – un type autonome qui a une seule valeur null.
+undefined pour les valeurs non attribuées – un type autonome avec une valeur unique undefined.
+symbol pour les identifiants uniques.
+Et un type de données non primitif :
+object pour des structures de données plus complexes. 
+*/
