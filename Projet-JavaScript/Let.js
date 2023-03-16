9a160ac3ef93c84506afc6592d19299e1fe53820
Ghack81 <herve-mention@hotmail.com>
2023-03-16 17:02:59 +0100

    Ajout d'un exemple de type de variable

--- Projet-JavaScript/Let.js
+++ Projet-JavaScript/Let.js
@@ -12,3 +12,14 @@ console.log(ad);
 // PROMPT
 var firstName = prompt("Quelle est votre nom ?");
 console.log(firstName);
+//-----------------------------other test--------------------//
+
+//Nous pouvons également déclarer plusieurs variables sur une seule ligne :
+let user = 'John', age = 25, message = 'Hello';
+console.log(user);
+
+//La variante multiligne est un peu plus longue, mais plus facile à lire:
+//let user = 'John';
+//let age = 25;
+//let message = 'Hello';
+
